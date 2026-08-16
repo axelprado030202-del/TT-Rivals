a6ba6eb3fb4ae9e7932d02c97932c76ec5a14be6
HamonApp <axel.prado030202@gmail.com>
2026-08-16 02:31:48 -0300

    Delete js directory

--- js/auth.js
+++
@@ -1,5 +0,0 @@
-import { supabase } from './supabase.js';
-export const signUpUser=({email,password,firstName,lastName,username})=>supabase.auth.signUp({email,password,options:{data:{first_name:firstName,last_name:lastName,username}}});
-export const signInUser=({email,password})=>supabase.auth.signInWithPassword({email,password});
-export const signOutUser=()=>supabase.auth.signOut();
-export async function getSession(){const {data,error}=await supabase.auth.getSession();if(error)throw error;return data.session;}
