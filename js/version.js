23f3eb0181d3e305dd9fc331a5006d6588ff302f
HamonApp <axel.prado030202@gmail.com>
2026-08-20 05:37:06 -0300

    Add files via upload

---
+++ js/version.js
@@ -0,0 +1,4 @@
+export const APP_VERSION='1.0.1';
+// Build interno P6.1: control de rendimiento visual + P6.
+export const APP_BUILD='v1.0.1-p6.1-visual-performance-controls';
+export const APP_CACHE_NAME='tt-rivals-v1-0-1-p6-1-visual-performance-controls';
