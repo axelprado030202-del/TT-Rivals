fee89e8ed4c43c9dbd8b456cff542a4c20388160
HamonApp <axel.prado030202@gmail.com>
2026-08-19 23:03:11 -0300

    Delete js directory

--- js/v56_stats.js
+++
@@ -1,7 +0,0 @@
-import {supabase} from './supabase.js';
-
-export async function getMyStatsV56(){
-  const {data,error}=await supabase.rpc('get_my_stats_v56');
-  if(error)throw error;
-  return data||{};
-}
