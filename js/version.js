5f0064f043576b53943065c26c69820edc9a0f59
HamonApp <axel.prado030202@gmail.com>
2026-08-20 05:35:35 -0300

    Delete js/version.js

--- js/version.js
+++
@@ -1,4 +0,0 @@
-export const APP_VERSION='1.0.1';
-// Build interno P6: interfaz animada + optimización + PWA icon refresh.
-export const APP_BUILD='v1.0.1-p6-visual-performance';
-export const APP_CACHE_NAME='tt-rivals-v1-0-1-p6-visual-performance';
